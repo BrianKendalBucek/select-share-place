021a15bb27e0fead8dc8fc8c3f0eaeed65d290e9
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-19 09:21:08 -0700

    added axios and axios api get function

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+import axios from "axios";
 
-const GOOGLE_API_KEY = 'AIzaSyD6PyTay39sgUB-git_8C5ysOkp81eSXE4';
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyD6PyTay39sgUB-git_8C5ysOkp81eSXE4";
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  // send this to Google's API!
-
+  axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+      enteredAddress
+    )}&key=${GOOGLE_API_KEY}`
+  )
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 
-form.addEventListener('submit', searchAddressHandler);
+form.addEventListener("submit", searchAddressHandler);
