edde26f6bc2bcfcfdd14367f109c03322986492e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-19 10:10:28 -0700

    got map working with marker via logic in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyD6PyTay39sgUB-git_8C5ysOkp81eSXE4";
 
+declare var google: any;
+
 type GoogleGeocodingResponse = {
-  results: {geometry: {location: {lat: number, lng: number}}}[];
-  status: 'OK' | 'ZERO_RESULTS';
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
 };
 
 function searchAddressHandler(event: Event) {
@@ -15,21 +17,27 @@ function searchAddressHandler(event: Event) {
   const enteredAddress = addressInput.value;
 
   axios
-  .get<GoogleGeocodingResponse>(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-      enteredAddress
-    )}&key=${GOOGLE_API_KEY}`
-  )
-  .then(response => {
-    if (response.data.status !== 'OK') {
-      throw new Error('Could not fetch location!');
-    }
-    const coordinates = response.data.results[0].geometry.location;
-  })
-  .catch(err => {
-    alert(err.message);
-    console.log(err);
-  });
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 8
+      });
+
+      new google.maps.Marker({position: coordinates, map: map});
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
