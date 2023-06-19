10c1bdff3b5f546ee0ffda113c225864f123b311
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-19 09:04:20 -0700

    added handler, event listener, and api key

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyD6PyTay39sgUB-git_8C5ysOkp81eSXE4';
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API!
+
+}
+
+form.addEventListener('submit', searchAddressHandler);
