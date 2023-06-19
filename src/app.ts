30b50ee6c3fda70457f04cee0f9a19d6d743d6a1
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-19 09:31:14 -0700

    added logic to use data from api based on input

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,29 @@ const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyD6PyTay39sgUB-git_8C5ysOkp81eSXE4";
 
+type GoogleGeocodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  axios.get(
+  axios
+  .get<GoogleGeocodingResponse>(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
       enteredAddress
     )}&key=${GOOGLE_API_KEY}`
   )
   .then(response => {
-    console.log(response);
+    if (response.data.status !== 'OK') {
+      throw new Error('Could not fetch location!');
+    }
+    const coordinates = response.data.results[0].geometry.location;
   })
   .catch(err => {
+    alert(err.message);
     console.log(err);
   });
 }
